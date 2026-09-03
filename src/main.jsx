3c9b507184f48ac7e1b90655d1dc1587114040a8
tpham15 <phamtrivienn@gmail.com>
2026-09-03 23:11:06 +0700

    Pilot final - non catalog disposition fix

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import SmartQuote from "./SmartQuote.jsx";
-import SupabaseAuthGate from "./supabase/SupabaseAuthGate.jsx";
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-  React.createElement(SupabaseAuthGate, null, (cloud) => React.createElement(SmartQuote, { cloud }))
-);
